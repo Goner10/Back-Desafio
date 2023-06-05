10e033b97a7bb1a27260dc713980a37c8d31dd2a
valenti94br <valenti94br@gmail.com>
2023-06-05 18:54:57 +0200

    campo de imagen añadido al modelo

--- Models/Event.js
+++ Models/Event.js
@@ -5,22 +5,32 @@ const EventSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    
     description: {
         type: String,
         required: true,
     },
+
     date: {
         type: Date,
         required: true,
     },
+
     attendees: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
     }],
+
     interest: {
         type: String,
         required: true,
     },
+
+    imageUrl: [{
+        type: String,
+        required: false,
+    }],
+    
 });
 
 const Event = mongoose.model('Event', EventSchema);
