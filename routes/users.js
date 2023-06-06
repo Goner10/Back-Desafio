293293c7082455e0aab7362724577e1bf1b17496
valenti94br <valenti94br@gmail.com>
2023-06-07 00:02:38 +0200

    logout funcionando

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post('/register', UserController.register);
 router.post('/login', UserController.login);
 router.get('/me', authentication, UserController.getInfo);
-router.post('/logout', authentication, UserController.logout);
+router.delete('/logout', authentication, UserController.logout);
 router.put('/update/:_id', [authentication, isAdmin], UserController.update);
 router.delete('/delete/:_id', [authentication, isAdmin], UserController.delete);
 
