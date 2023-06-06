039ff632e050190ea5d4bf836bc9bc03482d3903
valenti94br <valenti94br@gmail.com>
2023-06-07 00:13:43 +0200

    vamos por buen camino

--- Models/User.js
+++ Models/User.js
@@ -10,33 +10,42 @@ const UserSchema = new Schema({
     trim: true,
     minlength: 3
   },
+
   email: {
     type: String,
     required: true,
     unique: true,
     match: [/.+\@.+\..+/, 'Por favor ingrese un correo electrónico válido']
   },
+
   password: {
     type: String,
     required: true,
     minlength: 6
   },
+
   //Edem,alumno,profesor,por mostrar 
   typeUser: {
     type: String,
     require: false,
   },
+
   languages: [{
     type: String,
     required: false,
   }],
+
   course: {
     type: String,
     required: false,
   },
+
   interests: [{
     type: String,
   }],
+
+  role: { type: String, default: "user" },
+  
   tokens: [],
   dateCreated: {
     type: Date,
