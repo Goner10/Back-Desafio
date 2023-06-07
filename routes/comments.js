e54638e53e417f2db35cbee44c9b59dbc635f03c
adrianMartinezTeba <adrymate@gmail.com>
2023-06-07 17:10:43 +0200

    comentarios añadidos

--- routes/comments.js
+++ routes/comments.js
@@ -6,6 +6,7 @@ const { authentication } = require('../Middleware/authentication');
 const router = express.Router();
 
 router.post('/create', authentication, CommentController.create);
+//acordarse de tener en cuenta que para canal de difusion el crarlo solo podran admins y el modificarlo solo autor del canaldif o superadmin
 router.put('/update/:_id', authentication, CommentController.update);
 router.delete('/delete/:_id', authentication, CommentController.delete);
 
