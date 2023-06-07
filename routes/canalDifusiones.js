7f4f91512ca3a0401b2230ba1bf1c8569d9e893e
valenti94br <valenti94br@gmail.com>
2023-06-07 21:33:50 +0200

    rutas mejoradas

--- routes/canalDifusiones.js
+++ routes/canalDifusiones.js
@@ -3,10 +3,10 @@ const canalDifusionController = require('../controllers/CanalDifusionController'
 
 const router = express.Router();
 
-router.post('/', canalDifusionController.create);
-router.get('/', canalDifusionController.getAll);
-router.get('/:id', canalDifusionController.getById);
-router.put('/:id', canalDifusionController.update);
-router.delete('/:id', canalDifusionController.delete);
+router.post('/create', canalDifusionController.create);
+router.get('/all', canalDifusionController.getAll);
+router.get('/:_id', canalDifusionController.getById);
+router.put('/update/:id', canalDifusionController.update);
+router.delete('/delete/:id', canalDifusionController.delete);
 
 module.exports = router;
