ba351786317c2c9bb54d0f7140f3115a18834318
valenti94br <valenti94br@gmail.com>
2023-06-07 21:05:31 +0200

    arreglos

--- routes/canalDifusiones.js
+++ routes/canalDifusiones.js
@@ -3,19 +3,10 @@ const canalDifusionController = require('../controllers/CanalDifusionController'
 
 const router = express.Router();
 
-// Ruta para crear un canal de difusión
 router.post('/', canalDifusionController.create);
-
-// Ruta para obtener todos los canales de difusión
 router.get('/', canalDifusionController.getAll);
-
-// Ruta para obtener un canal de difusión por ID
 router.get('/:id', canalDifusionController.getById);
-
-// Ruta para actualizar un canal de difusión por ID
 router.put('/:id', canalDifusionController.update);
-
-// Ruta para eliminar un canal de difusión por ID
 router.delete('/:id', canalDifusionController.delete);
 
 module.exports = router;
