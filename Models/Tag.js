88b6f97128d6e4c68993fe3576e867a236e86b57
valenti94br <valenti94br@gmail.com>
2023-06-11 19:11:47 +0200

    tag arreglado

--- Models/Tag.js
+++ Models/Tag.js
@@ -1,15 +1,9 @@
 const mongoose = require('mongoose');
 
 const TagSchema = new mongoose.Schema({
-  id: {
-    type: [mongoose.Schema.Types.ObjectId],
-    default: [],
-    get: arr => arr.length > 0 ? arr[0] : null,
-    set: id => [id]
-  },
   name: {
-    type: [String],
-    default: []
+    type: String,
+    required: true
   }
 });
 
