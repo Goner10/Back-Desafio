a3d6af790487c1690bad3b818dea79108c6367c1
valenti94br <valenti94br@gmail.com>
2023-06-12 02:27:21 +0200

    tiene un cambio

--- routes/canalDifusiones.js
+++ routes/canalDifusiones.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const canalDifusionController = require('../controllers/CanalDifusionController');
-const { authentication, isAdmin,modCanDif } = require('../middleware/authentication');
+
+const { authentication, isAdmin, modCanDif } = require('../middleware/authentication');
 
 const router = express.Router();
 
-router.post('/create' ,canalDifusionController.create);
+router.post('/create', canalDifusionController.create);
 router.get('/all', canalDifusionController.getAll);
 router.get('/:id', canalDifusionController.getById);
-router.put('/update/:id',authentication,modCanDif ,canalDifusionController.update);
-router.delete('/delete/:id',authentication,modCanDif ,canalDifusionController.delete);
+router.put('/update/:id', authentication, modCanDif, canalDifusionController.update);
+router.delete('/delete/:id', authentication, modCanDif, canalDifusionController.delete);
 
 module.exports = router;
